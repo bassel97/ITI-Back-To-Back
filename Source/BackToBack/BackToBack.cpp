5910fff4c67e29e7e39494d1419d25a9a5265cc0
bassel97 <bassel.mks97@gmail.com>
2023-04-14 20:43:26 +0200

    Initial commit - Engine setup

---
+++ Source/BackToBack/BackToBack.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BackToBack.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BackToBack, "BackToBack" );
---
+++ Source/BackToBack/GameModes/BTBGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BTBGameModeBase.generated.h"
+
+/**
+ * Our Version of GameModeBase.
+ */
+UCLASS()
+class BACKTOBACK_API ABTBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
