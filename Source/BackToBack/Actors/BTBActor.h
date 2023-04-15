b70c95d3f63ad650b9a20529c660f0f598195441
yusefabbas-A <79419424+yusefabbas-A@users.noreply.github.com>
2023-04-16 00:36:26 +0200

    Added Our Actor Base class (BTBActor) in a folder called Actors

---
+++ Source/BackToBack/Actors/BTBActor.h
@@ -0,0 +1,20 @@
+// Copyright di-tri studio, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BTBActor.generated.h"
+
+
+/**
+ * Our Version of Actor.
+ */
+
+UCLASS()
+class BACKTOBACK_API ABTBActor : public AActor
+{
+
+	GENERATED_BODY()
+
+};
