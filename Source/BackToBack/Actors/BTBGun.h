9c048bd5470b6f7236a37bf287c8bbeaf3c38ba4
MostafaZewail <45624506+MostafaZewail@users.noreply.github.com>
2023-05-15 10:52:15 +0300

    BTBGun class created

---
+++ Source/BackToBack/Actors/BTBGun.h
@@ -0,0 +1,17 @@
+// Copyright di-tri studio, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BTBActor.h"
+#include "BTBGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACKTOBACK_API ABTBGun : public ABTBActor
+{
+	GENERATED_BODY()
+	
+};
