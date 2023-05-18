273ecdba225b4c684b928ca81830ef7c659a8f1d
yusefabbas-A <79419424+yusefabbas-A@users.noreply.github.com>
2023-05-18 11:53:59 +0300

    Some Fixes

--- Source/BackToBack/Actions/BTBCharacterSwitchAction.cpp
+++ Source/BackToBack/Actions/BTBCharacterSwitchAction.cpp
@@ -25,21 +25,32 @@ void UBTBCharacterSwitchAction::Act(ABTBCharacter* Character, const bool bIsAI)
 
 	if (!bIsAI)
 	{ 
-		if (Character->GetbStartSwitching() )
+		if (Character->GetbStartSwitching())
 		{
-			
-
 			if (!PlayableCharacter->bGunAttached)
 			{
+				UE_LOG(LogTemp, Warning, TEXT("firsh switch cond!!"));
+
 				if (myGun->GetbIsOverlapping())
 				{
+					UE_LOG(LogTemp, Warning, TEXT("Switching!!"));
+
 					myGun->GunMesh->SetSimulatePhysics(false);
 
 					myGun->AttachToComponent(PlayableCharacter->GetMesh()->GetAttachmentRoot(), FAttachmentTransformRules::KeepRelativeTransform, "GunSocket");
 
 					PlayableCharacter->bGunAttached = true;
-
+					
 				}
+			}
+			else if (PlayableCharacter->bGunAttached)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("ditch it!!"));
+				myGun->DetachFromActor(FDetachmentTransformRules::KeepRelativeTransform);
+				myGun->SetActorLocationAndRotation(PlayableCharacter->GetMesh()->GetSocketLocation("GunSocket"), PlayableCharacter->GetMesh()->GetSocketRotation("GunSocket"));
+				PlayableCharacter->bGunAttached = false;
+				//myGun->GunMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+				//myGun->GunMesh->SetSimulatePhysics(true);
 
 			}
 			
