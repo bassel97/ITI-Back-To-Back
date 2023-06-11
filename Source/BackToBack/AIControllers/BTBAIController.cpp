f8fcb318df4701db441b47a9cc33bb55e6ad8795
Ahmed5ameh <44699830+Ahmed5ameh@users.noreply.github.com>
2023-06-11 13:47:32 +0300

    Modified enemy AI for mini game 2

--- Source/BackToBack/AIControllers/BTBAIController.cpp
+++ Source/BackToBack/AIControllers/BTBAIController.cpp
@@ -10,18 +10,53 @@
 void ABTBAIController::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	FillPlayerActorsArray();
+	RunTheBehaviourTree();
+}
 
+void ABTBAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	UpdateBlackboardClosestPlayerVariable();
+}
+
+void ABTBAIController::FillPlayerActorsArray()
+{
 	const TObjectPtr<UWorld> World = GetWorld();
-	if(!ensure(World != nullptr && AIBehaviorTree != nullptr && PlayableCharacter != nullptr))
+	if(!ensure(World != nullptr))
 	{
 		return;
 	}
-
-	RunBehaviorTree(AIBehaviorTree);
 	
-	const TObjectPtr<AActor> PlayerActor = UGameplayStatics::GetActorOfClass(World, PlayableCharacter);
-	if(PlayerActor)
+	UGameplayStatics::GetAllActorsOfClass(World, ABTBPlayableCharacter::StaticClass(), PlayerActors);
+}
+
+void ABTBAIController::RunTheBehaviourTree()
+{
+	if(!ensure(AIBehaviorTree != nullptr))
 	{
-		GetBlackboardComponent()->SetValueAsObject("PlayerActor", PlayerActor);
+		return;
 	}
+	
+	RunBehaviorTree(AIBehaviorTree);
+}
+
+void ABTBAIController::UpdateBlackboardClosestPlayerVariable()
+{
+	if(!PlayerActors.IsEmpty())
+	{
+		const TObjectPtr<AActor> ClosestPlayer = GetClosestPlayer();
+		GetBlackboardComponent()->SetValueAsObject("PlayerActor", ClosestPlayer);
+	}
+}
+
+TObjectPtr<AActor> ABTBAIController::GetClosestPlayer()
+{
+	if(GetPawn()->GetDistanceTo(PlayerActors[0]) < GetPawn()->GetDistanceTo(PlayerActors[1]))
+	{
+		return PlayerActors[0];
+	}
+	return PlayerActors[1];
 }
